f2de3fd787482902bd46932c655f2fe98cb66674
Amir Zahari <amirzahari91@gmail.com>
2023-10-07 20:17:40 +0800

    Enf of section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   
@@ -23,7 +24,12 @@ import { HomeModule } from './home/home.module';
     HomeModule
   ],
 
-  providers: [],
+  providers: [
+    // add interceptor class here.
+    // angular come with bunchs of interceptor by their own, so, take it as multi: true
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
