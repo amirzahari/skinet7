920198fdab47b81f9d6048c4f0eb45ed6534bc00
Amir Zahari <amirzahari91@gmail.com>
2024-01-17 16:14:37 +0800

    End of section 18

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
 
@@ -29,7 +30,8 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
     // add interceptor class here.
     // angular come with bunchs of interceptor by their own, so, take it as multi: true
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
   ],
 
   bootstrap: [AppComponent]
